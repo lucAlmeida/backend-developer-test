7a3fef4e5e5cb8101cb3bee8e584934408f840a9
lucAlmeida <lucas.a.souza@outlook.com>
2020-07-30 22:20:26 -0300

    Spring Boot initial setup

---
+++ src/main/java/com/forleven/backenddevelopertest/BackendDeveloperTestApplication.java
@@ -0,0 +1,13 @@
+package com.forleven.backenddevelopertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDeveloperTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDeveloperTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forleven/backenddevelopertest/BackendDeveloperTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forleven.backenddevelopertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDeveloperTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
